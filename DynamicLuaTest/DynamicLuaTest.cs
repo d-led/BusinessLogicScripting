d04d8fcea7e839b6a3737da285dba10e1ba36e30
Dmitry Ledentsov <dmlled@yahoo.com>
2014-07-27 11:48:33 +0200

    testing exception handling

--- DynamicLuaTest/DynamicLuaTest.cs
+++ DynamicLuaTest/DynamicLuaTest.cs
@@ -34,5 +34,40 @@ namespace BusinessLogicScripting
             ((int)lua.logic.AnotherAnswer).Should().Be(logic.AnotherAnswer);
             ((int)lua("return logic.AnotherAnswer")[0]).Should().Be(logic.AnotherAnswer);
         }
+
+        [TestMethod]
+        public void ExceptionShouldBeThrownOnSyntaxError()
+        {
+            Action script = () => lua("a[)");
+            script.ShouldThrow<Exception>();
+        }
+
+        [TestMethod]
+        public void ExceptionShouldBeThrownOnRuntimeError()
+        {
+            Action script = () => lua("a()");
+            script.ShouldThrow<Exception>();
+        }
+
+        [TestMethod]
+        public void KnownCrashingBugsShouldBeControllable()
+        {
+            //http://www.lua.org/bugs.html
+
+            Action[] actions = new Action[] {
+                ()=>lua(@"table.unpack({}, 0, 2^31 - 1)"),
+                ()=>lua(@"function f() f() end f()"),
+                ()=>lua(@"print(string.find(string.rep('a', 2^20), string.rep('.?', 2^20)))"),
+                ()=>lua(@"print(unpack({1,2,3}, 2^31-1, 2^31-1))"),
+                ()=>lua(@"a = string.dump(function()return;end)
+                            a = a:gsub(string.char(30,37,122,128), string.char(34,0,0), 1)
+                            loadstring(a)()")
+            };
+
+            foreach (var action in actions)
+            {
+                action.ShouldThrow<Exception>();
+            }
+        }
     }
 }
