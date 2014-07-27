af5adcdb194d9373e782235299c03c34dba6c8dc
Dmitry Ledentsov <dmlled@yahoo.com>
2014-07-27 09:58:27 +0200

    dynamiclua intro

---
+++ BusinessLogicScripting/DynamicLuaTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace BusinessLogicScripting
+{
+    [TestClass]
+    public class DynamicLuaTest
+    {
+        [TestMethod]
+        public void SimpleScripting()
+        {
+            dynamic lua = new DynamicLua.DynamicLua();
+
+            lua("result = { a = 42, b = 33 }");
+
+            ((int)lua["result.a"]).Should().Be(42);
+            ((int)lua.result.a).Should().Be(42);
+        }
+    }
+}
---
+++ DomainLogic/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLogic
+{
+    public class DomainLogic
+    {
+        int TheAnswer()
+        {
+            return 42;
+        }
+
+        int AnotherAnswer
+        {
+            get { return 33; }
+        }
+    }
+}
